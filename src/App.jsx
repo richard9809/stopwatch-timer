90beafded46aaadd17a25c99d4a901e5a96faa19
Richard Mulu <richard.ndisya@strathmore.edu>
2023-10-04 17:41:36 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [isRunning, setIsRunning] = useState(false);
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+
+  useEffect(() => {
+    let interval;
+
+    if (isRunning) {
+      interval = setInterval(() => {
+        if (seconds === 59) {
+          setMinutes((prevMinutes) => prevMinutes + 1);
+          setSeconds(0);
+        } else {
+          setSeconds((prevSeconds) => prevSeconds + 1);
+        }
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [isRunning, seconds]);
+
+  const startTimer = () => {
+    setIsRunning(true);
+  };
+
+  const stopTimer = () => {
+    setIsRunning(false);
+  };
+
+  const resetTimer = () => {
+    setIsRunning(false);
+    setMinutes(0);
+    setSeconds(0);
+  };
+
+  return (
+    <div className="container">
+      <h1>Timer</h1>
+      <span> {minutes} mins </span>
+      <span> {seconds} secs</span>
+      <div>
+        <button onClick={startTimer}>Start</button>
+        <button onClick={stopTimer}>Stop</button>
+        <button onClick={resetTimer}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
